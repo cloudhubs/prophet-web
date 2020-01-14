6fa3aeaca75292b24d089944fae3aa611acc4238
Jan Svacina <jan@svacina.net>
2020-01-13 21:38:43 -0600

    vis dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import './App.css';
 import {render} from "react-dom";
+import TopAppBar from '@material/top-app-bar/component'
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import ResponsiveDrawer from "./ResponsiveDrawer";
 
 interface Props {}
 
@@ -39,7 +57,6 @@ class App extends React.Component<Props, State> {
 
     async onSubmit() {
         const response: any = await this.http;
-        console.log(response);
     }
 
     http = async () => {
@@ -54,7 +71,6 @@ class App extends React.Component<Props, State> {
                 }),
                 body: JSON.stringify(this.state.prophetRequest)
             });
-
             return response.ok;
         } catch (ex) {
             return false;
@@ -64,6 +80,9 @@ class App extends React.Component<Props, State> {
     render() {
         return (
             <div>
+                Drawer:
+                <ResponsiveDrawer/>
+
                 <p>{this.state.github}</p>
                 https://github.com/<input type="text" value={this.state.github} onChange={this.onChange}/>
                 <button type="submit" onClick={this.onSubmit}>Submit</button>
