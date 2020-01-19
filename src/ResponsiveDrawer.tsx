a9e60bb41e9143c2651232628cb6951338fcddd5
Jan Svacina <jan@svacina.net>
2020-01-18 23:54:00 -0600

    only ui component

--- src/ResponsiveDrawer.tsx
+++ src/ResponsiveDrawer.tsx
@@ -5,19 +5,20 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
 import FolderIcon from '@material-ui/icons/Folder';
 import { ListSubheader } from '@material-ui/core';
-import Graph from "react-graph-vis";
+import {ConfigurationForm} from "./components/ConfigurationForm";
+import {UMLClassDiagram} from "./components/UMLClassDiagram";
+import {CommunicationDiagram} from "./components/CommunicationDiagram";
+
 
 const drawerWidth = 240;
 
@@ -102,37 +103,6 @@ export default function ResponsiveDrawer(props: ResponsiveDrawerProps) {
     );
 
 
-    const graph = {
-        nodes: [
-            { id: 1, label: "Node 1", title: "node 1 tootip text", shape: "box" },
-            { id: 2, label: "Node 2", title: "node 2 tootip text" },
-            { id: 3, label: "Node 3", title: "node 3 tootip text" },
-            { id: 4, label: "Node 4", title: "node 4 tootip text" },
-            { id: 5, label: "Node 5", title: "node 5 tootip text" }
-        ],
-        edges: [
-            { from: 1, to: 2 },
-            { from: 1, to: 3 },
-            { from: 2, to: 4 },
-            { from: 2, to: 5 }
-        ]
-    };
-
-    const options = {
-        layout: {
-            hierarchical: true
-        },
-        edges: {
-            color: "#000000"
-        },
-        height: "500px"
-    };
-
-    const events = {
-        select: (event: any) => {
-            var { nodes, edges } = event;
-        }
-    };
 
     return (
         <div className={classes.root}>
@@ -144,17 +114,14 @@ export default function ResponsiveDrawer(props: ResponsiveDrawerProps) {
                         aria-label="open drawer"
                         edge="start"
                         onClick={handleDrawerToggle}
-                        className={classes.menuButton}
-                    >
-                        <MenuIcon />
+                        className={classes.menuButton}>
+                        <MenuIcon/>
                     </IconButton>
                     <Typography variant="h6" noWrap>
                         Prophet App
                     </Typography>
                 </Toolbar>
             </AppBar>
-
-
 
             <nav className={classes.drawer} aria-label="mailbox folders">
                 {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
@@ -189,36 +156,9 @@ export default function ResponsiveDrawer(props: ResponsiveDrawerProps) {
             </nav>
             <main className={classes.content}>
                 <div className={classes.toolbar} />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-
-                </Typography>
-
-                Graph:
-                <Graph
-                    graph={graph}
-                    options={options}
-                    events={events}
-                    getNetwork={()=> {
-                        //  if you want access to vis.js network api you can set the state in a parent component using this property
-                    }}
-                />
-
-                Mermaid:
-
-
-
+                <ConfigurationForm/>
+                <UMLClassDiagram/>
+                <CommunicationDiagram/>
             </main>
         </div>
     );
