0d65d9f468d831ce10493eba6d14640c7e678ef0
Jan Svacina <jan@svacina.net>
2020-01-22 12:16:33 -0600

    cleaning up app

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,18 @@
 import React, { StrictMode } from 'react';
 import './App.css';
 import ResponsiveDrawer from "./ui-elements/ResponsiveDrawer";
-import { GlobalStateProvider, useGlobalState } from './state';
+import { GlobalStateProvider } from './state';
 
-interface Props {}
-
-interface State {
-    github: string;
-    prophetAppUrl: string;
-    prophetRequest: ProphetRequest;
-};
-
-interface ProphetRequest {
-    url: string
-}
-
-class App extends React.Component<Props, State> {
-
-    state: State;
-
-    constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            github: '',
-            prophetAppUrl: "localhost:8080/",
-            prophetRequest: {
-                url: ''
-            }
-        };
-    }
-
+class App extends React.Component<{}> {
     render() {
         return (
             <div>
-
                 <StrictMode>
                     <GlobalStateProvider>
                         <ResponsiveDrawer/>
                     </GlobalStateProvider>
                 </StrictMode>
-
-
             </div>
-
-
         );
     }
 }
