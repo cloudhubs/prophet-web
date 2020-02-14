1cfed29cc3478ac1c942f450534ff15c8c57f83d
Jan Svacina <jan@svacina.net>
2020-02-14 11:35:36 -0600

    sending multiple repos

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,29 +3,42 @@ import {Mermaid} from "./Mermaid"
 import Diagram from "./Diagram"
 import {useGlobalState} from '../state';
 import ConfigurationForm from "./ConfigurationForm";
-import { Paper, Typography, Grid } from "@material-ui/core";
+import { Paper, Typography, Grid, Divider } from "@material-ui/core";
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 const Canvas = () => {
     const [prophetAppData, update] = useGlobalState('prophetAppData');
     const [selectedMs] = useGlobalState('ms');
     const [contextMap] = useGlobalState('contextMap');
     const [communication] = useGlobalState('communication');
+    const [loading] = useGlobalState('loading');
+    const useStyles = makeStyles((theme: Theme) =>
+        createStyles({
+            autoSizeInput: {
+                margin: '2px'
+            },
+            boxik: {
+                margin: theme.spacing(3, 3),
+            },
+            root: {
+                flexGrow: 1,
+                overflow: 'hidden',
+                padding: theme.spacing(0, 3),
+            }
+        }),
+    );
+    const classes = useStyles();
+
     return (
         <div>
             <ConfigurationForm/>
-
-            {/*<Mermaid*/}
-            {/*    chart={*/}
-            {/*        prophetAppData.global.contextMap} />*/}
-
-            {/*<Mermaid*/}
-            {/*    chart={*/}
-            {/*        prophetAppData.global.communication} />*/}
-            <Grid container spacing={3}>
+            <Grid container spacing={3} className={classes.root}>
                 <Grid item xs={12}>
                     <Typography variant="h4">
                         Global
                     </Typography>
+                    <Divider/>
+
                 </Grid>
                 <Grid item xs={12}>
                     <Typography variant="h5">
@@ -34,7 +47,7 @@ const Canvas = () => {
                 </Grid>
                 <Grid item xs={12}>
                     <Paper>
-                        <Mermaid chart={prophetAppData.global.contextMap}/>
+                        {!loading && <Mermaid chart={prophetAppData.global.contextMap}/>}
                     </Paper>
                 </Grid>
                 <Grid item xs={12}>
@@ -44,40 +57,24 @@ const Canvas = () => {
                 </Grid>
                 <Grid item xs={12}>
                     <Paper>
-                        <Mermaid chart={prophetAppData.global.communication}/>
+                    {!loading && <Mermaid chart={prophetAppData.global.communication}/>}
                     </Paper>
                 </Grid>
                 <Grid item xs={12}>
                     <Typography variant="h4">
                         Microservices
                     </Typography>
+                    <Divider/>
                 </Grid>
                 <Grid item xs={12}>
-                {prophetAppData.ms.map((ms, index) => {
+                {!loading && prophetAppData.ms.map((ms, index) => {
                         return <Diagram ms={ms} />
                     })
                 }
                 </Grid>
-                
             </Grid>
-            
-            
-            
 
-            {contextMap &&
-            <Mermaid
-                chart={
-                    prophetAppData.global.contextMap} />
-            }
 
-            {communication &&
-
-            <Mermaid
-                chart={
-                    prophetAppData.global.communication} />
-            }
-
-            
         </div>
     );
 }
--- src/components/ConfigList.tsx
+++ src/components/ConfigList.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
+import { IconButton, Tooltip, Fab, List, Divider } from '@material-ui/core';
+// import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
-import CommentIcon from '@material-ui/icons/Comment';
 import {useGlobalState} from "../state";
-import { IconButton, Tooltip } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import RepositoryForm from './RepositoryForm';
+import AddIcon from '@material-ui/icons/Add';
+import { ReqConfigSingle } from '../data/ReqConfigSingle';
+import NavigationIcon from '@material-ui/icons/Navigation';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,6 +21,12 @@ const useStyles = makeStyles((theme: Theme) =>
       // maxWidth: 410,
       backgroundColor: theme.palette.background.paper,
     },
+    fab: {
+      margin: theme.spacing(2),
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
   }),
 );
 
@@ -26,6 +34,8 @@ export default function ConfigList() {
   const classes = useStyles();
   const [checked, setChecked] = React.useState([0]);
   const [vConfigMultiple, uConfigMultiple] = useGlobalState('configMultiple');
+  const [vProphetAppData, uProphetAppData] = useGlobalState('prophetAppData');
+  const [vLoading, uLoading] = useGlobalState('loading');
 
   const handleToggle = (value: number) => () => {
     const currentIndex = checked.indexOf(value);
@@ -48,13 +58,47 @@ export default function ConfigList() {
       }
   }
 
+  const onAdd = () => {
+    let newVal = JSON.parse(JSON.stringify(vConfigMultiple));
+    newVal[newVal.length] = new ReqConfigSingle();
+    console.log(newVal);
+    uConfigMultiple(newVal);
+  }
+
+  const onAnalyze = async () => {
+    console.log(vConfigMultiple);
+    let val = true;
+    uLoading(val);
+    const requestBody = {
+      "repositories": JSON.parse(JSON.stringify(vConfigMultiple))
+    }
+    const response = await fetch('http://localhost:8080/', {
+      method: 'post',
+      body: JSON.stringify(requestBody),
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin':'*'
+       }
+    });
+    if (response != null){
+      const body = await response.json();
+      uProphetAppData(body);
+      console.log(body);
+      val = !val;
+      uLoading(val);
+    }
+  }
+
   return (
+    <>
     <List className={classes.root}>
 
       {vConfigMultiple.map((cm, index) => {
         const labelId = `checkbox-list-label-${index}`;
 
         return (
+          <>
           <ListItem key={index} role={undefined} dense button>
             {/* <ListItemIcon>
               <Checkbox
@@ -86,11 +130,28 @@ export default function ConfigList() {
                   <DeleteIcon/>
                 </IconButton>
               </Tooltip>
-              
+
             </ListItemSecondaryAction>
           </ListItem>
+          {index !== vConfigMultiple.length -1 &&
+            <Divider light/>
+          }
+          </>
+
         );
       })}
+
     </List>
+    <Tooltip title="More Repositories" aria-label="add">
+      <Fab color="primary" className={classes.fab} onClick={onAdd} size="small">
+        <AddIcon />
+      </Fab>
+    </Tooltip>
+
+    <Fab variant="extended" onClick={onAnalyze}>
+        <NavigationIcon className={classes.extendedIcon} />
+        Analyze
+      </Fab>
+    </>
   );
 }
