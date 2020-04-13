110b57dc5dece1144bc6b7a8821c33d17bdec13f
Jan Svacina <jan@svacina.net>
2020-04-13 01:10:45 -0500

    global styles for app

---
+++ src/prophetStyles.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Box, makeStyles, Paper, Typography, useTheme} from "@material-ui/core";
+
+
+const prophetStyles = makeStyles({
+    root: {
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px',
+        padding: '12px 30px',
+    },
+}
+
+);
+
+export default prophetStyles;
