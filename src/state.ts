fb44b712eb70edcc41580b4156138528024681de
Jan Svacina <jan@svacina.net>
2020-04-20 21:08:56 -0500

    added errors state

--- src/state.ts
+++ src/state.ts
@@ -1,72 +1,8 @@
 import { createGlobalState } from 'react-hooks-global-state';
 import {ProphetAppData} from "./data/ProphetAppData";
-import {Configuration} from "./data/Configuration";
 import {ReqConfigSingle} from "./data/ReqConfigSingle";
-import { createStore } from "react-hooks-global-state";
-
-
-// type Action =
-//   | { type: "setMultipleConfigRepo", index: number, repository: string }
-//   | { type: "decrement" };
-
-// const reducer = (state = initialState, action: Action) => {
-//     switch (action.type) {
-//         case "setMultipleConfigRepo":
-//         return {
-//             ...state,
-//             backendUrl: action.repository,
-//             configMultiple: [
-
-//             ]
-//         };
-//         case "decrement":
-//         return {
-//             ...state
-//         };
-//         default:
-//         return state;
-//     }
-// };
-
-
-
-// interface InitState {
-//     backendUrl: string;
-//     organization: string
-//     repository: string;
-//     communicationGraph: string;
-//     prophetAppData: any;
-//     ms: string;
-//     contextMap: boolean;
-//     communication: boolean;
-//     // configMultiple: [new ReqConfigSingle(0), new ReqConfigSingle(1)],
-//     configMultiple: any[],
-//     configSingle: any
-//     isConfigSingle: boolean
-// }
-
-// const initialState: InitState = {
-//     backendUrl: 'localhost:8080',
-//     organization: 'cloudhubs',
-//     repository: 'tms',
-//     communicationGraph: `graph LR;
-//         A-->B;
-//         B-->C;
-//         B-->D[name];
-//       `,
-//     prophetAppData: data,
-//     ms: "",
-//     contextMap: false,
-//     communication: false,
-//     // configMultiple: [new ReqConfigSingle(0), new ReqConfigSingle(1)],
-//     configMultiple: [],
-
-//     configSingle: {},
-//     isConfigSingle: true
-// }
-
-// export const { dispatch, useGlobalState } = createStore(reducer, initialState);
-
+import {AnalysisErrors} from "./data/AnalysisErrors";
+import {ErrorItem} from "./data/ErrorItem";
 
 const data: ProphetAppData = {
     global: {
@@ -86,24 +22,14 @@ const data: ProphetAppData = {
     isMonolith: false
 }
 
-// const data: ProphetAppData = {
-//     global: {
-//         projectName: "TMS",
-//         communication: ``,
-//         contextMap: ``,
-//     },
-//     ms: [
-//         {
-//             name: "EMS",
-//             boundedContext: ``,
-//         },
-//         {
-//             name: "CMS",
-//             boundedContext: ``,
-//         },
-//     ],
-//     isMonolith: false
-// }
+function getAnalysisErrors() {
+    let analysisErrors = new AnalysisErrors();
+    let annotationError = new ErrorItem("No supported annotations found");
+    let javaError = new ErrorItem("Java code not found");
+    analysisErrors.errors.push(annotationError);
+    analysisErrors.errors.push(javaError);
+    return analysisErrors;
+}
 
 export const { useGlobalState } = createGlobalState({
     backendUrl: 'localhost:8080',
@@ -122,4 +48,5 @@ export const { useGlobalState } = createGlobalState({
     configMultiple: [new ReqConfigSingle(), new ReqConfigSingle()],
     configSingle: {},
     isConfigSingle: true,
+    analysisError: getAnalysisErrors()
 });
