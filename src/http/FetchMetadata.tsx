93bd4ac83edb66d7e4339175c64f558847aad064
Jan Svacina <jan@svacina.net>
2020-04-22 23:37:10 -0500

    git api impl

--- src/http/FetchMetadata.tsx
+++ src/http/FetchMetadata.tsx
@@ -1,6 +1,55 @@
 
+const GIT_API = 'https://api.github.com/';
 
 const FetchMetadata = {
+
+    async getAllReposFromOrganization(organization: string) {
+        const response = await fetch( GIT_API + 'users/' + organization +  '/repos', {
+            method: 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        });
+        if (response != null){
+            const body = await response.json();
+            //ToDo: trigger action
+        } else {
+            console.log("server error");
+        }
+    },
+
+    async getOrganizationDetail(organization: string) {
+        const response = await fetch( GIT_API + 'users/' + organization, {
+            method: 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        });
+        if (response != null){
+            const body = await response.json();
+            //ToDo: trigger action
+        } else {
+            console.log("server error");
+        }
+    },
+
+    async getRepositoryDetail(organization: string, repository: string) {
+        const response = await fetch(GIT_API + 'repos/' + organization +  '/' + repository, {
+            method: 'GET',
+            headers : {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        });
+        if (response != null){
+            const body = await response.json();
+            //ToDo: trigger action
+        } else {
+            console.log("server error");
+        }
+    }
 
 }
 
