6f6889c0ae6b239c2fa0eb79527b9cd2e1fcb816
Jan Svacina <jan@svacina.net>
2020-05-06 23:50:09 -0500

    components

---
+++ src/components/configuration/SingleError.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SingleError = ({conf: ReqConfigSingle}) => {
+
+    const header = (
+        <>
+
+        </>
+    );
+
+    const footer = (
+        <>
+
+        </>
+    );
+
+    return (
+        <React.Fragment>
+            {header}
+
+            {footer}
+        </React.Fragment>
+    )
+}
+export default SingleConfig;
