7d8c55f85a8d2c4a37ee636f82a0d92e8b2e8611
Jan Svacina <jan@svacina.net>
2020-05-17 01:34:02 -0500

    diagram components

--- src/components/canvas/Diagrams.tsx
+++ src/components/canvas/Diagrams.tsx
@@ -1,15 +1,26 @@
 import React from "react";
+import {useGlobalState} from "../../state/appState";
+import GlobalDiagrams from "./GlobalDiagrams";
+import MsDiagram from "./MsDiagram";
+import {Ms} from "../../model/Ms";
 
 /**
- *
+ * Passes global and microservice variables from state to stateless
+ * components
+ * @variables Global and Ms object from State
+ * @stateful
  * @constructor
  *
  */
 const Diagrams = () => {
 
+    const [global] = useGlobalState('global');
+    const [ms] = useGlobalState('ms');
+    //ToDo: loading
     return (
         <React.Fragment>
-
+            <GlobalDiagrams global={global}/>
+            {ms.map((m: Ms) => (<MsDiagram ms={m}/>))}
         </React.Fragment>
     )
 }
