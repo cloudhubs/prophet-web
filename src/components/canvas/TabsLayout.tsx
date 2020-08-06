fe12e6d0525a52bb70382e34608d7a0da9582f97
Jan Svacina <jan@svacina.net>
2020-08-05 22:30:44 -0500

    tab layout

---
+++ src/components/canvas/TabsLayout.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import {Card} from "@material-ui/core";
+import Diagrams from "./Diagrams";
+import Communication from "./Communication";
+import {useGlobalState} from "../../state/appState";
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    dir?: string;
+    index: any;
+    value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: any) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        backgroundColor: theme.palette.background.paper,
+        marginBottom: '20px'
+        //width: 500,
+    },
+}));
+
+export default function TabsLayout() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+    const [global] = useGlobalState('global');
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index: number) => {
+        setValue(index);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="default">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="fullWidth"
+                    aria-label="full width tabs example"
+                >
+                    <Tab label="Communication" {...a11yProps(0)} />
+                    <Tab label="Context Map" {...a11yProps(1)} />
+                    <Tab label="Inconsistencies" {...a11yProps(2)} />
+                    <Tab label="Code clones" {...a11yProps(3)} />
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={value}
+                onChangeIndex={handleChangeIndex}
+            >
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                    <Communication global={global}/>
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    <Diagrams/>
+                </TabPanel>
+                <TabPanel value={value} index={2} dir={theme.direction}>
+                    Item Three
+                </TabPanel>
+                <TabPanel value={value} index={3} dir={theme.direction}>
+                    Item Four
+                </TabPanel>
+            </SwipeableViews>
+        </div>
+    );
+}
